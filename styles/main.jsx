59201c75b8cb4284808c08f64f2ce66bf0d29176
bruno muzzi <bmuzzi15@gmail.com>
2026-08-27 19:40:05 -0300

    criando app,main e json

---
+++ styles/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./app.jsx";
+import "./styles/style.css";
+
+createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    
+);
+
